1d0d74df9943b1f33eb130d38a49766d443c2ddf
Kat Lea <katarina.lea@globacore.com>
2023-03-07 13:33:16 -0500

    set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { Session, SessionContextProvider } from '@supabase/auth-helpers-react'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session
+}>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
+
+export default MyApp
